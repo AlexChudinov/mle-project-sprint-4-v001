3bc473f7c9698a67fb8b8333762ba827794a4534
Алексей Чудинов <chudinov@inbox.ru>
2025-03-07 15:57:23 +0300

    fix ssh_copy

--- scripts/ssh_copy.sh
+++ scripts/ssh_copy.sh
@@ -2,15 +2,15 @@ USER=mle-user
 VM_IP=84.201.139.92
 
 function sync_dir_from() {
-    LOCAL_DIR=$1
-    REMOTE_DIR=$2
-    rsync -v --progress ${USER}@${VM_IP}:${REMOTE_DIR} ${LOCAL_DIR}
+    REMOTE_DIR=$1
+    LOCAL_DIR=$2
+    rsync -av --progress ${USER}@${VM_IP}:${REMOTE_DIR} ${LOCAL_DIR}
 }
 
 function sync_dir_to() {
     LOCAL_DIR=$1
     REMOTE_DIR=$2
-    rsync -v --progress ${LOCAL_DIR} ${USER}@${VM_IP}:${REMOTE_DIR}
+    rsync -av --progress ${LOCAL_DIR} ${USER}@${VM_IP}:${REMOTE_DIR}
 }
 case $1 in
     --to)
